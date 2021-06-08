cb2e512f75e6ad2a9d76a27bfb68b7cb341a17d3
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-08 23:02:34 +0200

    Added mountain stuff

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -20,23 +20,83 @@ public class TerrainGeneration : MonoBehaviour
 
     [SerializeField] float safeZoneHeight = 0.3f;
     [SerializeField] AnimationCurve safezoneTransitionCurve;
+    [SerializeField] float steepnessTransitionFactor;
+    [SerializeField] AnimationCurve grassTypeTransition;
+    float[,] heightmap;
 
     [NaughtyAttributes.Button]
     public void RegenerateTerrain()
     {
+        if (data == null) return;
 
         var random = new System.Random(seed);
+        heightmap = GenerateHeights(random);
+        GenerateSplatmap(random);
+
+        data.SetHeights(0, 0, heightmap);
+    }
+
+    private void GenerateSplatmap(System.Random random)
+    {
+        int res = data.alphamapResolution;
+        float[,,] splatmap = new float[res, res, 3];
+
+
+        for (int x = 0; x < res; x++)
+        {
+            for (int y = 0; y < res; y++)
+            {
+                float xPercent = (float)x / res;
+                float yPercent = (float)y / res;
+                float steepness = Mathf.Clamp01((GetSteepnessAt(xPercent, yPercent) - 1.5f) * 0.5f);
+
+                float grass = 1 - steepness;
+                float h = grassTypeTransition.Evaluate(GetHeightAt(xPercent, yPercent));
+
+                splatmap[x, y, 0] = grass * h;
+                splatmap[x, y, 1] = steepness;
+                splatmap[x, y, 2] = grass * (1 - h);
+            }
+        }
+
+        data.SetAlphamaps(0, 0, splatmap);
+    }
+
+    private float GetHeightAt(float x, float y)
+    {
+        float res = data.heightmapResolution;
+        int xCoord = Mathf.FloorToInt(x * res);
+        int yCoord = Mathf.FloorToInt(y * res);
+        return heightmap[xCoord, yCoord];
+    }
+
+    private float GetSteepnessAt(float x, float y)
+    {
+        if (x > 1 || y > 1 || x < 0 || y < 0) return 0;
+
+        float res = data.heightmapResolution;
+
+        int xCoord = Mathf.FloorToInt(x * res);
+        int yCoord = Mathf.FloorToInt(y * res);
+
+        float hp = heightmap[xCoord, yCoord];
+        float hpr = heightmap[xCoord + 1, yCoord];
+        float hpu = heightmap[xCoord, yCoord + 1];
+        float hpru = heightmap[xCoord + 1, yCoord + 1];
+
+        float angle = Mathf.Abs(Mathf.Asin((hpr + hpu + hpru) * 0.33333f - hp) * res);
+        return angle;
+    }
+
+    private float[,] GenerateHeights(System.Random random)
+    {
         int res = data.heightmapResolution;
-        int halfRes = res / 2;
         float[,] heightmap = new float[res, res];
 
-
-        Vector2 center = new Vector2(halfRes, halfRes);
         float offsetX = (float)random.NextDouble();
         float offsetY = (float)random.NextDouble();
 
         float totalnoiselayersIntensity = noiseLayers.Sum((x) => x.Intensity);
-
 
         for (int x = 0; x < res; x++)
         {
@@ -59,14 +119,6 @@ public class TerrainGeneration : MonoBehaviour
                 heightmap[x, y] = height;
             }
         }
-
-        data.SetHeights(0, 0, heightmap);
-
-
+        return heightmap;
     }
-
-
-
-
-
 }
